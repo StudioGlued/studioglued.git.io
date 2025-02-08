06f1e783d1e0d434468fffd863350739d9098e28
StudioGlued <rbadhasha@gmail.com>
2025-02-08 01:30:43 +0000

    4.5

---
+++ script.js
@@ -0,0 +1,20 @@
+const elements = document.querySelectorAll(".scroll-in")
+
+window.addEventListener("scroll", () => {
+    const innerHeightOfWindow = window.innerHeight;
+
+    elements.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < innerHeightOfWindow){
+            box.classList.add("show")
+        } 
+        
+        else {
+            box.classList.remove("show")
+        }
+
+        })
+    })
+
+    
